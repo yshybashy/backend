0c6c6f9678aac5ef8fb9f6c3ee1a55ceb452c0c8
Daniel Ishibashï <brdanielbr@gmail.com>
2020-09-17 19:38:33 -0300

    corrigido porta do mongoDB

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/ipag', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/ipag', {useNewUrlParser: true, useUnifiedTopology: true});
 
 requireDir('./src/models');
 
-//app.use('/sistema', require('./src/routes/routes'));
+app.use('/sistema', require('./src/routes/routes'));
 app.listen(process.env.PORT || 3005);
